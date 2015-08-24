b895c560f33d10f0cec97bbc336e8b80e6df2e7e
Iris Shao <irisshao@Iriss-MacBook-Pro.local>
2015-08-23 22:08:23 -0700

    Added a template file and modifed the views.py to render new template

--- aboutme_project/aboutme_app/views.py
+++ aboutme_project/aboutme_app/views.py
@@ -1,5 +1,20 @@
 # Create your views here.
-from django.http import HttpResponse
+
+# From Chapter 3. Django Basics (Tango with Django)
+# from django.http import HttpResponse
+
+# def index(request):
+# 	return HttpResponse("Hello world!")
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Hello world!")
+	# Request the context of the request
+	context = RequestContext(request)
+
+	# Construct a dictionary to pass to the template engine as its context.
+	context_dict = {'boldmessage': "I am bold font from the context"}
+
+	#Return a rendered response to send to the client.
+	return render_to_response('aboutme_app/index.html', context_dict, context)
--- aboutme_project/aboutme_project/settings.py
+++ aboutme_project/aboutme_project/settings.py
@@ -1,5 +1,14 @@
 # Django settings for aboutme_project project.
 
+# Import the OS module and work out our project's paths
+import os
+SETTINGS_DIR = os.path.dirname(__file__)
+
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -111,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'code/aboutme_project/templates'
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
