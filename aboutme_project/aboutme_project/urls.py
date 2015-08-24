024851400f5c7194bf3da12079827c9541aa602a
Iris Shao <irisshao@Iriss-MacBook-Pro.local>
2015-08-23 21:41:36 -0700

    Created a view and added relevant urls.py

--- aboutme_project/aboutme_project/urls.py
+++ aboutme_project/aboutme_project/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^aboutme_app/', include('aboutme_app.urls')), # Adding new tuple
 )
