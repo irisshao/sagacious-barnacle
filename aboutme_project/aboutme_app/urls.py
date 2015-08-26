a98061c9fa2a9cedcaadac9c1ff7f59692cef8ee
Iris Shao <irisshao@Iriss-MacBook-Pro.local>
2015-08-26 15:56:21 -0700

    Finished(?) index2.html and index2.css and began work on the about me page

--- aboutme_project/aboutme_app/urls.py
+++ aboutme_project/aboutme_app/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from aboutme_app import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index2'))
+	url(r'^$', views.index, name='index2'),
+	url(r'^about/$', views.about, name='about'),
+)
--- aboutme_project/aboutme_app/views.py
+++ aboutme_project/aboutme_app/views.py
@@ -18,3 +18,23 @@ def index(request):
 
 	#Return a rendered response to send to the client.
 	return render_to_response('aboutme_app/index2.html', context_dict, context)
+
+def about(request):
+	# Request the context of the request
+	context = RequestContext(request)
+
+	# Construct a dictionary to pass to the template engine as its context.
+	context_dict = {'boldmessage': "I am bold font from the context"}
+
+	#Return a rendered response to send to the client.
+	return render_to_response('aboutme_app/about.html', context_dict, context)
+
+def resume(request):
+	# Request the context of the request
+	context = RequestContext(request)
+
+	# Construct a dictionary to pass to the template engine as its context.
+	context_dict = {'boldmessage': "I am bold font from the context"}
+
+	#Return a rendered response to send to the client.
+	return render_to_response('aboutme_app/resume.html', context_dict, context)
